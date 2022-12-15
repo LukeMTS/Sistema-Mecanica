2f5e739bebcdef665aaba7d5dbf88312e2084512
Lucas Mateus <lucas.mateus@orbitaldev.com.br>
2022-12-15 19:21:43 -0300

    inicio da rodada

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <Lukz />
+</template>
+
+<script>
+import Lukz from './Lukz.vue'
+
+    export default {
+        mounted() {
+            // alert('Component mounted.')
+        },
+        components: {
+            Lukz
+        }
+    }
+</script>
---
+++ resources/js/components/Lukz.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">oioio</div>
+</template>
+<script>
+
+    export default {
+        mounted() {
+            
+        },
+        name: "Lukz"
+    }
+</script>
